67a60578ebc6a58a45a607e1a1c79615abc5e905
FinFin <FinFin1351@gmail.com>
2024-07-20 03:19:21 +0900

    feat(handle, registry): setup infrastructure for session management and ChatLists
    - Added chatList DB table in handle to store sessionID, otherHandle, lastMessageTime, and lastViewedTime.
    - Implemented UpdateChatList and GetChatList handlers in handle to manage chat lists.
    - Added sessions DB table in registry to store session information.
    - Implemented QuerySession and EstablishSession handlers in registry for session management.
    - Updated Spawned handler in registry to handle session spawn success and update handles with new sessions.

--- lua/handle.lua
+++ lua/handle.lua
@@ -1,5 +1,28 @@
 local json = require("json")
 local profiles = {}
+REGISTRY_PROCESS_ID = "oH5zaOmPCdCL_N2Mn79qqwtoCLXS2y6gcXv7Ohfmh-k"
+local sqlite3 = require("lsqlite3")
+DB = DB or sqlite3.open_memory()
+
+DB:exec [[
+  CREATE TABLE IF NOT EXISTS chatList (
+    sessionID TEXT PRIMARY KEY,
+    otherHandle TEXT NOT NULL,
+    lastMessageTime INTEGER,
+    lastViewedTime INTEGER
+  );
+]]
+local function authorizeAndReply(msg, expected, errorMessage, reply)
+    if msg.From ~= expected then
+        print(errorMessage)
+        reply(json.encode({
+            status = "error",
+            message = "Unauthorized"
+        }))(msg)
+        return false
+    end
+    return true
+end
 
 Handlers.add(
     "ProfileUpdate",
@@ -37,5 +60,67 @@ Handlers.add(
     Handlers.utils.hasMatchingTag("Action", "GetProfile"),
     function(msg)
         Handlers.utils.reply(json.encode(profiles))(msg)
+    end
+)
+
+Handlers.add(
+    "UpdateChatList",
+    Handlers.utils.hasMatchingTag("Action", "UpdateChatList"),
+    function(msg)
+        local data = json.decode(msg.Data)
+
+        if not authorizeAndReply(msg, REGISTRY_PROCESS_ID, 'Unauthorized attempt to update chat list', Handlers.utils.reply) then
+            return
+        end
+
+        local stmt = DB:prepare [[
+          REPLACE INTO chatList (sessionID, otherHandle) VALUES (:sessionID, :otherHandle);
+        ]]
+
+        if not stmt then
+            error("Failed to prepare SQL statement: " .. DB:errmsg())
+        end
+
+        stmt:bind_names({
+            sessionID = data.sessionID,
+            otherHandle = data.otherHandle
+            -- lastMessageTime = data.lastMessageTime,
+            -- lastViewedTime = data.lastViewedTime
+        })
+
+        stmt:step()
+        stmt:reset()
+
+        print('Chat list updated with new session: ' .. data.sessionID)
+        Handlers.utils.reply(json.encode({ status = "success" }))(msg)
+    end
+)
+
+
+Handlers.add(
+    "GetChatList",
+    Handlers.utils.hasMatchingTag("Action", "GetChatList"),
+    function(msg)
+        -- if not authorizeAndReply(msg, ao.env.Process.Tags["MostAO-Handle-Owner"], 'Unauthorized attempt to get chat list', Handlers.utils.reply) then
+        --     return
+        -- end
+
+        local stmt = DB:prepare [[
+          SELECT sessionID, otherHandle, lastMessageTime, lastViewedTime FROM chatList;
+        ]]
+
+        if not stmt then
+            error("Failed to prepare SQL statement: " .. DB:errmsg())
+        end
+
+        local chatList = {}
+        for row in stmt:nrows() do
+            table.insert(chatList, row)
+        end
+
+        stmt:reset()
+        print('ChatList: ' .. json.encode(chatList))
+        Handlers.utils.reply(json.encode(chatList))(msg)
+        print('Chat list sent to owner')
     end
 )
--- lua/registry.lua
+++ lua/registry.lua
@@ -1,9 +1,32 @@
 local json = require("json")
 local sqlite3 = require("lsqlite3")
 local SQLITE_WASM64_MODULE = "u1Ju_X8jiuq4rX9Nh-ZGRQuYQZgV2MKLMT3CZsykk54"
-local HANDLE_LUA_CODE = [[
+local HANDLE_LUA_CODE = [=[
 local json = require("json")
 local profiles = {}
+REGISTRY_PROCESS_ID = "oH5zaOmPCdCL_N2Mn79qqwtoCLXS2y6gcXv7Ohfmh-k"
+local sqlite3 = require("lsqlite3")
+DB = DB or sqlite3.open_memory()
+
+DB:exec [[
+  CREATE TABLE IF NOT EXISTS chatList (
+    sessionID TEXT PRIMARY KEY,
+    otherHandle TEXT NOT NULL,
+    lastMessageTime INTEGER,
+    lastViewedTime INTEGER
+  );
+]]
+local function authorizeAndReply(msg, expected, errorMessage, reply)
+    if msg.From ~= expected then
+        print(errorMessage)
+        reply(json.encode({
+            status = "error",
+            message = "Unauthorized"
+        }))(msg)
+        return false
+    end
+    return true
+end
 
 Handlers.add(
     "ProfileUpdate",
@@ -43,7 +66,70 @@ Handlers.add(
         Handlers.utils.reply(json.encode(profiles))(msg)
     end
 )
-]]
+
+Handlers.add(
+    "UpdateChatList",
+    Handlers.utils.hasMatchingTag("Action", "UpdateChatList"),
+    function(msg)
+        local data = json.decode(msg.Data)
+
+        if not authorizeAndReply(msg, REGISTRY_PROCESS_ID, 'Unauthorized attempt to update chat list', Handlers.utils.reply) then
+            return
+        end
+
+        local stmt = DB:prepare [[
+          REPLACE INTO chatList (sessionID, otherHandle) VALUES (:sessionID, :otherHandle);
+        ]]
+
+        if not stmt then
+            error("Failed to prepare SQL statement: " .. DB:errmsg())
+        end
+
+        stmt:bind_names({
+            sessionID = data.sessionID,
+            otherHandle = data.otherHandle
+            -- lastMessageTime = data.lastMessageTime,
+            -- lastViewedTime = data.lastViewedTime
+        })
+
+        stmt:step()
+        stmt:reset()
+
+        print('Chat list updated with new session: ' .. data.sessionID)
+        Handlers.utils.reply(json.encode({ status = "success" }))(msg)
+    end
+)
+
+
+Handlers.add(
+    "GetChatList",
+    Handlers.utils.hasMatchingTag("Action", "GetChatList"),
+    function(msg)
+        -- if not authorizeAndReply(msg, ao.env.Process.Tags["MostAO-Handle-Owner"], 'Unauthorized attempt to get chat list', Handlers.utils.reply) then
+        --     return
+        -- end
+
+        local stmt = DB:prepare [[
+          SELECT sessionID, otherHandle, lastMessageTime, lastViewedTime FROM chatList;
+        ]]
+
+        if not stmt then
+            error("Failed to prepare SQL statement: " .. DB:errmsg())
+        end
+
+        local chatList = {}
+        for row in stmt:nrows() do
+            table.insert(chatList, row)
+        end
+
+        stmt:reset()
+        print('ChatList: ' .. json.encode(chatList))
+        Handlers.utils.reply(json.encode(chatList))(msg)
+        print('Chat list sent to owner')
+    end
+)
+
+]=]
 DB = DB or sqlite3.open_memory()
 
 DB:exec [[
@@ -51,6 +137,14 @@ DB:exec [[
     handle TEXT PRIMARY KEY,
     pid TEXT NOT NULL,
     owner TEXT NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS sessions (
+    session_id TEXT PRIMARY KEY,
+    handleA TEXT NOT NULL,
+    handleB TEXT NOT NULL,
+    FOREIGN KEY (handleA) REFERENCES handles(handle),
+    FOREIGN KEY (handleB) REFERENCES handles(handle)
   );
 ]]
 
@@ -61,6 +155,18 @@ local function query(stmt)
     end
     stmt:reset()
     return rows
+end
+
+local function authorizeAndReply(msg, expected, errorMessage, reply)
+    if msg.From ~= expected then
+        print(errorMessage)
+        reply(json.encode({
+            status = "error",
+            message = "Unauthorized"
+        }))(msg)
+        return false
+    end
+    return true
 end
 
 Handlers.add(
@@ -146,30 +252,84 @@ Handlers.add(
 
         print('Spawned Done!')
 
-        local stmt = DB:prepare [[
-      REPLACE INTO handles (handle, pid, owner) VALUES (:handle, :pid, :owner);
-    ]]
+        if handle then
+            local stmt = DB:prepare [[
+              REPLACE INTO handles (handle, pid, owner) VALUES (:handle, :pid, :owner);
+            ]]
 
-        if not stmt then
-            error("Failed to prepare SQL statement: " .. DB:errmsg())
+            if not stmt then
+                error("Failed to prepare SQL statement: " .. DB:errmsg())
+            end
+
+            stmt:bind_names({
+                handle = handle,
+                pid = pid,
+                owner = owner
+            })
+            local result = ao.send({
+                Target = pid,
+                Action = "Eval",
+                Data = HANDLE_LUA_CODE
+            })
+            print("result: " .. json.encode(result))
+            stmt:step()
+            stmt:reset()
+            Handlers.utils.reply("Handle Spawn Success")(msg)
+        else
+            local handleA_name = msg.Tags["Session-HandleA-Name"]
+            local handleA_pid = msg.Tags["Session-HandleA-Process"]
+            local handleB_name = msg.Tags["Session-HandleB-Name"]
+            local handleB_pid = msg.Tags["Session-HandleB-Process"]
+
+            local stmt = DB:prepare [[
+              INSERT INTO sessions (session_id, handleA, handleB) VALUES (:session_id, :handleA, :handleB);
+            ]]
+
+            if not stmt then
+                error("Failed to prepare SQL statement: " .. DB:errmsg())
+            end
+
+            stmt:bind_names({
+                session_id = pid,
+                handleA = handleA_name,
+                handleB = handleB_name
+            })
+            stmt:step()
+            stmt:reset()
+            print('Session registered between ' .. handleA_name .. ' and ' .. handleB_name .. ': ' .. pid)
+            local transferedData = json.encode({
+                session_id = pid,
+                otherHandle = handleB_name,
+            })
+            print('Transfering data to ' .. handleA_pid .. ': ' .. transferedData)
+            ao.send({
+                Target = handleA_pid,
+                Action = "UpdateChatList",
+                Data = json.encode({
+                    sessionID = pid,
+                    otherHandle = handleB_name,
+                    -- lastMessageTime = os.time(),
+                    -- lastViewedTime =
+                    --     os.time()
+                })
+            })
+
+            ao.send({
+                Target = handleB_pid,
+                Action = "UpdateChatList",
+                Data = json.encode({
+                    sessionID = pid,
+                    otherHandle = handleA_name,
+                    -- lastMessageTime = os.time(),
+                    -- lastViewedTime =
+                    --     os.time()
+                })
+            })
+            Handlers.utils.reply("Session Spawn Success")(msg)
         end
-
-        stmt:bind_names({
-            handle = handle,
-            pid = pid,
-            owner = owner
-        })
-        local result = ao.send({
-            Target = pid,
-            Action = "Eval",
-            Data = HANDLE_LUA_CODE
-        })
-        print("result: " .. json.encode(result))
-        stmt:step()
-        stmt:reset()
-        Handlers.utils.reply("Handle Spawn Success")(msg)
     end
 )
+
 
 Handlers.add(
     "Renounce",
@@ -211,5 +371,122 @@ Handlers.add(
         stmt:reset()
         print('Renounce Done!')
         Handlers.utils.reply(json.encode({ status = "success" }))(msg)
+    end
+)
+
+Handlers.add(
+    "QuerySession",
+    Handlers.utils.hasMatchingTag("Action", "QuerySession"),
+    function(msg)
+        local data = json.decode(msg.Data)
+
+        local stmt = DB:prepare [[
+          SELECT session_id FROM sessions WHERE (handleA = :handleA AND handleB = :handleB) OR (handleA = :handleB AND handleB = :handleA);
+        ]]
+
+        if not stmt then
+            error("Failed to prepare SQL statement: " .. DB:errmsg())
+        end
+
+        stmt:bind_names({
+            handleA = data.handleA,
+            handleB = data.handleB
+        })
+
+        local rows = query(stmt)
+
+        if #rows == 0 then
+            Handlers.utils.reply(json.encode({ status = "error", message = "No session found" }))(msg)
+            print('No session found between ' .. data.handleA .. ' and ' .. data.handleB)
+        else
+            Handlers.utils.reply(json.encode({ status = "success", sessionID = rows[1].session_id }))(msg)
+            print('Session found between ' .. data.handleA .. ' and ' .. data.handleB)
+        end
+    end
+)
+
+Handlers.add(
+    "EstablishSession",
+    Handlers.utils.hasMatchingTag("Action", "EstablishSession"),
+    function(msg)
+        local data = json.decode(msg.Data)
+        local handleA = data.handleA
+        local handleB = data.handleB
+
+        print("Handle A: " .. handleA)
+        print("Handle B: " .. handleB)
+
+        local stmtA = DB:prepare [[
+          SELECT pid, owner FROM handles WHERE handle = :handleA;
+        ]]
+        local stmtB = DB:prepare [[
+          SELECT pid, owner FROM handles WHERE handle = :handleB;
+        ]]
+
+        if not stmtA or not stmtB then
+            error("Failed to prepare SQL statement: " .. DB:errmsg())
+        end
+
+        stmtA:bind_names({ handleA = handleA })
+        stmtB:bind_names({ handleB = handleB })
+
+        local rowsA = query(stmtA)
+        local rowsB = query(stmtB)
+
+        if #rowsA == 0 or #rowsB == 0 then
+            Handlers.utils.reply(json.encode({ status = "error", message = "Handle not found" }))(msg)
+            print(rowsA)
+            print(rowsB)
+            print('One or both handles not found in registry')
+            return
+        end
+
+        local processA = rowsA[1].pid
+        local ownerA = rowsA[1].owner
+        local processB = rowsB[1].pid
+        local ownerB = rowsB[1].owner
+
+        if not authorizeAndReply(msg, ownerA, 'Unauthorized attempt to establish session for handleA', Handlers.utils.reply) then
+            return
+        end
+
+        if not authorizeAndReply(msg, ownerB, 'Unauthorized attempt to establish session for handleB', Handlers.utils.reply) then
+            return
+        end
+
+        local queryStmt = DB:prepare [[
+          SELECT session_id FROM sessions WHERE (handleA = :handleA AND handleB = :handleB) OR (handleA = :handleB AND handleB = :handleA);
+        ]]
+
+        if not queryStmt then
+            error("Failed to prepare SQL statement: " .. DB:errmsg())
+        end
+
+        queryStmt:bind_names({
+            handleA = handleA,
+            handleB = handleB
+        })
+
+        local existingSession = query(queryStmt)
+
+        if #existingSession > 0 then
+            Handlers.utils.reply(json.encode({ status = "success", sessionID = existingSession[1].session_id }))(msg)
+            print('Session already exists between ' ..
+                handleA .. ' and ' .. handleB .. ': ' .. existingSession[1].session_id)
+            return
+        end
+
+        local spawnMessage = {
+            Tags = {
+                { name = "Session-HandleA-Name",    value = handleA },
+                { name = "Session-HandleA-Process", value = processA },
+                { name = "Session-HandleB-Name",    value = handleB },
+                { name = "Session-HandleB-Process", value = processB },
+                { name = "Name",                    value = "MostAO-Session" }
+            }
+        }
+        local result = ao.spawn(SQLITE_WASM64_MODULE, spawnMessage)
+        print('Spawn session request sent for handles ' .. handleA .. ' and ' .. handleB)
+        Handlers.utils.reply(json.encode({ status = "success", message = "Session spawn request sent, " }))(msg)
     end
 )
